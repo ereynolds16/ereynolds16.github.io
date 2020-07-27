b7843032a9ea1ba5eb0142064570934ea1c03e52
ereynolds16 <edward.reynolds@gmail.com>
2020-07-26 20:22:20 -0400

    Updated code to include pictures and pop-up windows for all ballclubs except those we have not officially visited.

--- baseball/resources/javascript/script.js
+++ baseball/resources/javascript/script.js
@@ -1,4 +1,44 @@
-let washingtonButton = document.getElementById('washington');
+const locations = ['washington', 'pittsburgh', 'cleveland', 'detroit', 'chicagocubs', 'stlouis', 'kansascity', 'minnesota', 'toronto', 'boston',
+ 'philadelphia', 'baltimore', 'miami', 'tampa', 'atlanta', 'denver', 'houston', 'arlington', 'phoenix', 'sandiego', 'losangeles', 'anaheim', 
+ 'seattle', 'oakland', 'sanfrancisco', 'yankees', 'cincinnati', 'chicagosox', 'milwaukee', 'mets']
+let buttons = [];
+let modals = [];
+let closes = [];
+for (let i=0; i < 30; i++) {
+    let locName = locations[i];
+    console.log(locName);
+    buttons.push(locations[i]);
+    console.log(buttons);
+    modals.push(locations[i]+'_modal');
+    console.log(modals);
+    closes.push(locations[i]+'_close');
+    //console.log(closes);
+    //let 'locName'+'Button' = document.getElementById(locName);
+    //console.log(locName+'Button');
+}
+
+for (let i=0; i < 30; i++) {
+    buttons[i] = document.getElementById(buttons[i]);
+    console.log(buttons);
+    modals[i] = document.getElementById(modals[i]);
+    console.log(modals);
+    closes[i] = document.getElementById(closes[i]);
+    console.log(closes);
+}
+
+for (let i=0; i < 30; i++) {
+    buttons[i].onclick = () => {
+        modals[i].style.display = "block";
+    }
+    closes[i].onclick = () => {
+        modals[i].style.display = "none";
+    }
+}
+
+
+
+//Code below here works to create a single box for the Washington Nationals. Use it as a guide to create boxes for everyone else.
+/*let washingtonButton = document.getElementById('washington');
 console.log(washingtonButton);
 let washingtonModal = document.getElementById('washington_modal');
 console.log(washingtonModal);
